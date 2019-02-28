30fb511904e04ec1f03ccc182a36758204a4cfd1
vucat2000 <cnikolai@earthlink.net>
2019-02-28 18:20:36 -0500

    version 2

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  var categories = ["math","science","space","nasa","hamster","computer"]; 
+
+  //dynamically create buttons for all of the categories
+
+  //build the query string and ajax
+
+});
